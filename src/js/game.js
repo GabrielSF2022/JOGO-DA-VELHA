4392f61a6bf692eb75e6ac4701d68dd470c7eb02
GABRIEL DE SOUZA FURTADO <gs294860@gmail.com>
2022-06-02 21:19:16 -0300

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -1,16 +1,18 @@
-let board = ['','','','','','','','',''];
+let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
-let symbols = ['o','x'];
+let symbols = ['o', 'x'];
 
-function handleMove(position){
+function handleMove(position) {
 
-    board[position] = symbols[playerTime];
+    if (board[position] == '') {
+        board[position] = symbols[playerTime]
 
-    if (playerTime == 0){
-        playerTime = 1;
-    }
+        if (playerTime == 0) {
+            playerTime = 1;
+        }
 
-    else {
-        playerTime = 0;
+        else {
+            playerTime = 0;
+        }
     }
 }
